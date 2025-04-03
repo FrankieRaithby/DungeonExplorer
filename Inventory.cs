b68d84be8f2de2977580923296ac498cd028cc8f
Frankie <frankieraithby@gmail.com>
2025-04-03 10:14:48 +0100

    Add items attribute

--- Inventory.cs
+++ Inventory.cs
@@ -1,7 +1,25 @@
-﻿namespace DungeonExplorer
+﻿using System.Collections.Generic;
+
+namespace DungeonExplorer
 {
     internal class Inventory
     {
+        private List<Item> _items;
+        public Inventory(List<Item> items)
+        {
+            _items = items;
+        }
+
+        public List<Item> Items
+        {
+            get { return _items; }
+            set { _items = value; }
+        }
+
+
+
+
+
 
     }
 }

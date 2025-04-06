9c09a9aaa2ae20ec6d7d34d4cd5915889f9ea2db
Frankie Raithby <frankieraithby@gmail.com>
2025-04-06 20:59:40 +0100

    Add constructor & methods to Item Class

--- Item.cs
+++ Item.cs
@@ -2,6 +2,49 @@
 {
     internal class Item
     {
+        /// <summary>
+        /// Private Fields.
+        /// </summary>
+        private string _name;
+        private string _description;
 
+        /// <summary>
+        /// Parameterized Constructor.
+        /// </summary>
+        public Item(string name, string description)
+        {
+            _name = name;
+            _description = description;
+        }
+
+        /// <summary>
+        /// Public properties for accessing private fields.
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        /// <summary>
+        /// Returns name of Item instance.
+        /// </summary>
+        public string GetName()
+        {
+            return Name;
+        }
+
+        /// <summary>
+        /// Returns description of Item instance.
+        /// </summary>
+        public string GetDescription()
+        {
+            return Description;
+        }
     }
 }

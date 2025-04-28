d8dfe32dfebbe4cf7981028977da4d1e5944fd58
Frankie Raithby <frankieraithby@gmail.com>
2025-04-28 12:50:55 +0100

    Armour Class

---
+++ Armour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    internal class Armour : Item
+    {
+        private string _type;
+        private int _defence;
+        
+        public Armour(string name, string description, int weight, string type, int defence) : base(name, description, weight)
+        {
+            _type = type;
+            _defence = defence;
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public int Defence
+        {
+            get { return _defence; }
+            set { _defence = value; }
+        }
+    }
+}
